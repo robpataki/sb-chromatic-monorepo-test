24e586988273d5784a09977e32d2bc7213034086
Rob Pataki <robert@robertpataki.com>
2025-04-23 11:13:43 +0100

    Add Storybook to Project B

--- apps/project-b/.storybook/main.ts
+++ apps/project-b/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/nextjs",
